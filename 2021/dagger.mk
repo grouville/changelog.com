eb4cef65ecfcf0dda89c6f1c3f918443d51abb02
Gerhard Lazu <gerhard@lazu.co.uk>
2021-11-08 08:43:13 +0000

    Take Dagger for a CI spin
    This went well and generated a few questions. Even though this is not
    complete and is currrently failing, learning in public sounds like a
    great fit for this. Current issues/questions:
    
    - Does the existence of the .dagger dir imply dagger init?
      Should dagger init be idempotent instead?
    - How do we split our test step into multiple steps, with caching for deps & compilation?
    - How do we spin another container for PostgreSQL, and tear it down when done?
      Reference: https://github.com/thechangelog/changelog.com/blob/fb661d0cf3a4db731d46ef7f1cec44a5d1f4581a/.circleci/config.yml#L55
    
    This may be unrelated, but this is a new failure that I have not seen
    before:
    
        8:39AM INF prod | #5 35.27 cc -O2 -fPIC -std=c99 -Wall -Ic_src -I/usr/local/lib/erlang/erts-12.0.1/include -shared c_src/blocks.o c_src/buffer.o c_src/cmark.o c_src/cmark_ctype.o c_src/commonmark.o c_src/houdini_href_e.o c_src/houdini_html_e.o c_src/houdini_html_u.o c_src/html.o c_src/inlines.o c_src/iterator.o c_src/latex.o c_src/man.o c_src/node.o c_src/references.o c_src/render.o c_src/scanners.o c_src/utf8.o c_src/xml.o src/cmark_nif.c -o priv/cmark.so
        8:39AM INF prod | #5 35.37 c_src/blocks.o: file not recognized: file format not recognized
        8:39AM INF prod | #5 35.37 collect2: error: ld returned 1 exit status
        8:39AM INF prod | #5 35.37 make: *** [Makefile:73: priv/cmark.so] Error 1
        8:39AM INF prod | #5 35.37 could not compile dependency :cmark, "mix compile" failed. You can recompile this dependency with "mix deps.compile cmark", update it with "mix deps.update cmark" or clean it with "mix deps.clean cmark"
        8:39AM INF prod | #5 35.37 ==> changelog
        8:39AM INF prod | #5 35.37 ** (Mix) Could not compile with "make" (exit status: 2).
        8:39AM INF prod | #5 35.37 You need to have gcc and make installed. If you are using
        8:39AM INF prod | #5 35.37 Ubuntu or any other Debian-based system, install the packages
        8:39AM INF prod | #5 35.37 "build-essential". Also install "erlang-dev" package if not
        8:39AM INF prod | #5 35.37 included in your Erlang/OTP version. If you're on Fedora, run
        8:39AM INF prod | #5 35.37 "dnf group install 'Development Tools'".
        8:39AM INF prod | #5 35.37
        8:39AM ERR prod | canceled    duration=38.1s
        8:39AM INF prod | #5 35.62 yarn install v1.22.10
        8:39AM INF prod | #5 35.74 [1/4] Resolving packages...
        8:39AM INF prod | #5 36.18 [2/4] Fetching packages...
        8:39AM FTL system | failed to up environment: task failed: process "/bin/sh -c   mix do deps.get, deps.compile, test" did not complete successfully: exit code: 1
    
    How to run this?
    
        cd 2021
        . .env
        make dagger-ci
    
    Signed-off-by: Gerhard Lazu <gerhard@lazu.co.uk>

--- 2021/dagger.mk
+++ 2021/dagger.mk
@@ -8,9 +8,9 @@ DAGGER_DIR := $(LOCAL_BIN)/dagger_v$(DAGGER_VERSION)_$(platform)_amd64
 DAGGER_URL := $(DAGGER_RELEASES)/download/v$(DAGGER_VERSION)/$(notdir $(DAGGER_DIR)).tar.gz
 DAGGER := $(DAGGER_DIR)/dagger
 $(DAGGER): | $(GH) $(CURL) $(LOCAL_BIN)
-	@printf "$(BOLD)$(RED)curl will fail until repository is private$(NORMAL)\n"
-	@printf "$(RED)$(CURL) --progress-bar --fail --location --output $(DAGGER_DIR).tar.gz $(DAGGER_URL)$(NORMAL)\n\n"
-	@printf "$(BOLD)Using $(GREEN)gh$(NORMAL) $(BOLD)instead$(NORMAL)\n"
+	@printf "🤔 $(RED)$(BOLD)curl$(RESET)$(RED) will fail while $(BOLD)$(DAGGER_RELEASES)$(RESET)$(RED) is a private repository$(RESET)\n"
+	@printf "$(GREY)$(CURL) --progress-bar --fail --location --output $(DAGGER_DIR).tar.gz $(DAGGER_URL)$(RESET)\n"
+	@printf "💡 $(GREEN)Using $(BOLD)gh$(RESET)$(GREEN) instead$(RESET)\n"
 	$(GH) release download v$(DAGGER_VERSION) --repo dagger/dagger --pattern '*darwin_amd64.tar.gz' --dir $(LOCAL_BIN)
 	mkdir -p $(DAGGER_DIR) && tar zxf $(DAGGER_DIR).tar.gz -C $(DAGGER_DIR)
 	touch $(DAGGER)
@@ -22,3 +22,37 @@ dagger: $(DAGGER)
 .PHONY: releases-dagger
 releases-dagger:
 	$(OPEN) $(DAGGER_RELEASES)
+
+DAGGER_CTX := cd $(BASE_DIR) && $(DAGGER) --log-format plain
+DAGGER_HOME := $(BASE_DIR)/.dagger
+DAGGER_ENV := $(DAGGER_HOME)/env
+
+$(DAGGER_HOME): $(DAGGER)
+	@printf "\n🤔 $(YELLOW)Does the existence of the $(RESET)$(BOLD).dagger$(RESET)$(YELLOW) dir imply $(GREEN)$(BOLD)dagger init$(RESET)?\n"
+	@printf "💡 $(GREEN)Make this command idempotent$(RESET)\n\n"
+	$(DAGGER_CTX) init
+.PHONY: dagger-init
+dagger-init: $(DAGGER_HOME)
+
+# 💡 This would have been nicer:
+# dagger env ci || dagger env ci ...
+# An idempotent command would have been the nicest though 😉
+$(DAGGER_ENV)/ci: | dagger-init
+	$(DAGGER_CTX) new ci --package $(CURDIR)/dagger/ci
+
+define _convert_dockerignore_to_excludes
+awk '{ print "--exclude " $$1 }' < $(BASE_DIR)/.dockerignore
+endef
+.PHONY: dagger-ci
+dagger-ci: $(DAGGER_ENV)/ci
+	$(DAGGER_CTX) input dir source . $(shell $(_convert_dockerignore_to_excludes))
+	@printf "\n🤔 $(YELLOW)In repos like this one, uploading source to a remote $(BOLD)BUILDKIT_HOST$(RESET)$(YELLOW) is slow: $(BOLD)392s$(RESET)$(YELLOW) for $(BOLD)3.4GB$(RESET)\n"
+	@printf "$(YELLOW)   Even cached, the source operation is still slow: $(BOLD)27s$(RESET)\n"
+	@printf "💡 $(GREEN)Introduce $(BOLD).daggerignore$(RESET)$(GREEN) and/or respect $(BOLD).dockerignore$(RESET)\n\n"
+	@printf "⭐️ $(GREEN)Multiple $(BOLD)--exclude$(RESETT)$(GREEN) statements worked well - it's something that I intend to document$(RESET)\n"
+	@printf "🙌 $(GREEN)My uncached ci source now takes $(BOLD)6s$(RESET)$(GREEN) for $(BOLD)80MB$(RESET)$(GREEN) & $(BOLD)0.5s$(RESET)$(GREEN) cached$(RESET)\n\n"
+	$(DAGGER_CTX) up
+
+.PHONY: dagger-clean
+dagger-clean:
+	rm -fr $(DAGGER_HOME)
---
+++ 2021/dagger/ci/main.cue
@@ -0,0 +1,41 @@
+// Starting point: https://docs.dagger.io/1012/ci
+// 💡 Formatting differs because https://github.com/jjo/vim-cue auto formats - go fmt FTW!
+package main
+
+import (
+	"alpha.dagger.io/dagger"
+	"alpha.dagger.io/os"
+	"alpha.dagger.io/docker"
+)
+
+source: dagger.#Artifact
+
+// 🤔 How do we split this in multiple steps, with caching for deps & compilation?
+// 🤔 How do we spin another container for PostgreSQL, and tear it down when done?
+// Starting point: .circleci/config.yml
+test: os.#Container & {
+	image: docker.#Pull & {
+		from: "thechangelog/runtime:2021-05-29T10.17.12Z"
+	}
+	mount: "/app": from: source
+	command: """
+		  mix do deps.get, deps.compile, test
+		"""
+	env:
+		MIX_ENV: "test"
+	dir: "/app"
+}
+
+prod: os.#Container & {
+	image: docker.#Pull & {
+		from: "thechangelog/runtime:2021-05-29T10.17.12Z"
+	}
+	mount: "/app": from: source
+	command: """
+		  mix do deps.get, deps.compile
+		  cd assets && yarn install --frozen-lockfile
+		"""
+	env:
+		MIX_ENV: "prod"
+	dir: "/app"
+}
